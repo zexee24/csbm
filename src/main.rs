de3ef1c0270ba3e8d29506475ce4004475e5bce1
Jere Hasu <31950088+zexee24@users.noreply.github.com>
2023-04-07 00:46:28 +0300

    Now parses things, next to fix the list

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
-use html_parser::{Dom, Element, Node};
+use html_parser::{Dom, Element};
 use std::collections::hash_map::HashMap;
 mod food_item;
 
 use food_item::FoodItem;
 
-static BASE_URL: &str = "https://starbounder.org/";
+static BASE_URL: &str = "https://starbounder.org";
 fn main() {
-    let fil: Vec<FoodItem>;
-    let cache: HashMap<String, FoodItem> = HashMap::new();
-    for child in get_food_list().unwrap() {
-        let a = child.attributes;
+    let mut fil: Vec<FoodItem>= vec![];
+    let mut cache: HashMap<String, FoodItem> = HashMap::new();
+    let fl = get_food_list().unwrap();
+    let l = fl.len();
+    for (n, child) in fl.iter().enumerate(){
+        println!("Processing item {n}/{l}");
+        let a = &child.attributes;
         let name = a.get("title").unwrap().as_ref().unwrap();
         let href = a.get("href").unwrap().as_ref().unwrap();
+        let item = match cache.get(name){
+            Some(i) => i.clone(),
+            None => {
+                let (fi,c) = get_item_info(name, href, cache).unwrap();
+                cache = c;
+                fi
+            },
+        };
+        fil.push(item);
     }
+    println!("{:#?}", fil);
 }
 
 fn get_food_list() -> Option<Vec<Element>> {
@@ -23,7 +36,7 @@ fn get_food_list() -> Option<Vec<Element>> {
     let dom = Dom::parse(&b).unwrap();
     if let Some(i) = dom.children.first() {
         return match parse_childs_until(i.element().unwrap().clone(), |e| {
-            e.name == "ul".to_string()
+            e.name == *"ul"
         }) {
             Some(e) => Some(
                 e.children
@@ -53,7 +66,7 @@ fn parse_childs_until(d: Element, predicate: fn(&Element) -> bool) -> Option<Ele
     } else {
         for e in d.children {
             if let Some(el) = e.element() {
-                if let Some(e) = parse_childs_until(el.clone(), predicate.clone()) {
+                if let Some(e) = parse_childs_until(el.clone(), predicate) {
                     return Some(e);
                 }
             }
@@ -63,14 +76,14 @@ fn parse_childs_until(d: Element, predicate: fn(&Element) -> bool) -> Option<Ele
 }
 
 fn get_item_info(
-    name: String,
-    url: String,
+    name: &str,
+    url: &str,
     mut cache: HashMap<String, FoodItem>,
 ) -> Option<(FoodItem, HashMap<String, FoodItem>)> {
-    match cache.get(&url) {
+    match cache.get(url) {
         Some(f) => Some((f.clone(), cache)),
         None => {
-            let b = reqwest::blocking::get(BASE_URL.to_string() + &url)
+            let b = reqwest::blocking::get(BASE_URL.to_string() + url)
                 .ok()?
                 .text()
                 .ok()?;
@@ -79,8 +92,7 @@ fn get_item_info(
             let v = parse_childs_until(elem.clone(), |e| {
                 e.classes.contains(&"vector_info_footer".to_string())
             })?;
-            let ingredients = parse_ingredients(
-                parse_childs_until(elem, |e| {
+            let (ingredients, c) = match parse_childs_until(elem, |e| {
                     e.attributes
                         .get("style")
                         .map(|s| s.clone() == Some("width:300px;".to_string()))
@@ -102,16 +114,18 @@ fn get_item_info(
                             .children
                             .iter()
                             .any(|ec2| ec2.element().unwrap().children.iter().any(|ec3| ec3.text() == Some("INGREDIENTS")))
-                })?,
-                cache.clone(),
-            );
+                }) {
+                    Some(i) => parse_ingredients(i, cache.clone()),
+                    None => (vec![], cache.clone()),
+                };
+            cache = c;
             let p = find_price(v)?;
             let fi = FoodItem {
-                name: name.clone(),
+                name: name.to_string(),
                 price: p,
-                ingredients: vec![],
+                ingredients,
             };
-            cache.insert(name, fi.clone());
+            cache.insert(name.to_string(), fi.clone());
             Some((fi, cache))
         }
     }
@@ -127,8 +141,8 @@ fn find_price(e: Element) -> Option<usize> {
     None
 }
 
-fn parse_ingredients(element: Element, cache: HashMap<String, FoodItem>)
-/*-> Option<(Vec<(usize, FoodItem)>, HashMap<String, FoodItem>)>*/
+fn parse_ingredients(element: Element,mut cache: HashMap<String, FoodItem>)
+    -> (Vec<(usize, FoodItem)>, HashMap<String, FoodItem>)
 {
     let csb: Vec<Element> = element
         .children
@@ -137,7 +151,7 @@ fn parse_ingredients(element: Element, cache: HashMap<String, FoodItem>)
         .map(|e| e.to_owned())
         .filter(|e| e.classes.contains(&"crafting_stat_bg".to_string()))
         .collect();
-    let t: Vec<(usize, String, String)> = csb.iter().filter_map(|e| {
+    let pil: Vec<(usize, String, String)> = csb.iter().filter_map(|e| {
         e.children.iter().find(|e1| {
             e1.element().unwrap().attributes.get("style").map(|e2| {
                 e2.clone()
@@ -162,10 +176,19 @@ fn parse_ingredients(element: Element, cache: HashMap<String, FoodItem>)
                 }
             }
         }
-        (amount.unwrap().unwrap(), name.unwrap(), href.unwrap()).to_owned()
+        (amount.unwrap().unwrap(), name.unwrap(), href.unwrap())
     }).collect();
-
-    println!("{:#?}", t);
+    let fil: Vec<(usize, FoodItem)> = pil.iter().map(|item| {
+        match cache.get(&item.1){
+            Some(f) => (item.0,f.clone()),
+            None => {
+                let (fi, c) = get_item_info(&item.1, &item.2, cache.clone()).unwrap();
+                cache = c;
+                (item.0 , fi)
+            },
+        }
+    }).collect();
+    (fil, cache)
 }
 
 #[cfg(test)]
@@ -174,10 +197,11 @@ mod tests {
 
     #[test]
     fn test_get_item_info() {
-        let n = "Cooked Ribs".to_string();
-        let url = "Cooked_Ribs".to_string();
-        let (fi, _) = get_item_info(n.clone(), url, HashMap::new()).unwrap();
+        let n = "Cooked Ribs";
+        let url = "/Cooked_Ribs";
+        let (fi, c) = get_item_info(n, url, HashMap::new()).unwrap();
         assert_eq!(fi.name, n);
         assert_eq!(fi.price, 40);
+        println!("{:#?}", c)
     }
 }
